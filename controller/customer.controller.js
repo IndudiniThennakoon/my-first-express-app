7f3e2ff0a605c64e8bde5e6676bea0a56e9fe355
IndudiniThennakoon <thennakoonindudini@gmail.com>
2022-12-08 14:41:57 +0530

    chore(cust.controller) :create controller func

---
+++ controller/customer.controller.js
@@ -0,0 +1,26 @@
+const mysqlConnection = require("../config/config");
+
+
+// create controller functions - way 1
+function getAll(req,res,next){
+
+};
+
+// create controller functions - way 2
+const updateCustomer = (req,res,next)=>{
+
+}
+
+const deleteCustomer = (req,res,next)=>{
+
+}
+
+const addCustomer = (req,res,next)=>{
+
+}
+
+const searchCustomer = (req,res,next)=>{
+
+}
+
+
