c7aed879b9fd8db33f09d606a05c27b5278f6b5e
IndudiniThennakoon <thennakoonindudini@gmail.com>
2022-12-08 13:53:25 +0530

    create customer route using post method

--- routes/customer.route.js
+++ routes/customer.route.js
@@ -3,7 +3,12 @@ const router = require('express').Router();
 
 router.get('/getAll', async (req,res,next)=>{
     res.send("{mesg:'hello',name:'indu'}");
-})
+});
+
+router.post('/add', async (req,res,next)=>{
+    res.send("successfully added");
+});
+
 
 
 module.exports = router;
