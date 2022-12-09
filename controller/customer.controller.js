2e8dc5e75892fd77ef62d91dc56d5e182b54b616
IndudiniThennakoon <thennakoonindudini@gmail.com>
2022-12-09 12:02:21 +0530

    feat(cust.controller): getAll customer details

--- controller/customer.controller.js
+++ controller/customer.controller.js
@@ -1,33 +1,30 @@
 const mysqlConnection = require("../config/config");
 
-
 // create controller functions - way 1
-function getAll(req,res,next){
-    console.log("ewwwwwwwwwwwwww");
-
-};
+function getAll(req, res, next) {
+    mysqlConnection.query("SELECT * FROM CustomerTable",(err,rows,feild)=>{
+        if(!err){
+            res.send(rows);
+        }else{
+            console.log(err);
+        }
+    });
+ 
+}
 
 // create controller functions - way 2
-const updateCustomer = (req,res,next)=>{
+const updateCustomer = (req, res, next) => {};
 
-}
+const deleteCustomer = (req, res, next) => {};
 
-const deleteCustomer = (req,res,next)=>{
+const addCustomer = (req, res, next) => {};
 
-}
-
-const addCustomer = (req,res,next)=>{
-
-}
-
-const searchCustomer = (req,res,next)=>{
-
-}
+const searchCustomer = (req, res, next) => {};
 
 module.exports = {
-    getAll,
-    addCustomer,
-    deleteCustomer,
-    updateCustomer,
-    searchCustomer
+  getAll,
+  addCustomer,
+  deleteCustomer,
+  updateCustomer,
+  searchCustomer,
 };
